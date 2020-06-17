95b27e73aea899d57c1d89aa368de9b6fc30d81b
httam <66425946+httam@users.noreply.github.com>
2020-06-17 23:39:19 +0200

    Delete data_base.py

--- data_base.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd
-
-df = pd.read_html("https://coinmarketcap.com/currencies/bitcoin/historical-data/?start=20130429&end=20200520.html")[2]
-df.to_excel('table.xlsx', index=False)
-print('bitcoin data saved to table.xlsx')
